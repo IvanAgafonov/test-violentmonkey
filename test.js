7faede7263a0fc597779e712e977bf17ce399fce
Ivan <sudden1306@gmail.com>
2024-10-22 05:49:53 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         test script
+// @version      0.0
+// @namespace    Violentmonkey Scripts
+// @author       ivan
+// @match        https://www.google.com/*
+// @grant        none
+// @downloadURL  https://github.com/IvanAgafonov/raw/main/test-violentmonkey/test.js
+// @updateURL    https://github.com/IvanAgafonov/raw/main/test-violentmonkey/test.js
+// @homepage     https://github.com/IvanAgafonov
+// ==/UserScript==
+
+
+
+alert('Hello world!');
