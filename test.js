cb8fbb68302ffb0acff0f510fb8dcb229cfbe5ea
Ivan <sudden1306@gmail.com>
2024-10-22 06:03:05 +0300

    Update test.js

--- test.js
+++ test.js
@@ -5,6 +5,7 @@
 // @author       ivan
 // @match        https://www.google.com/*
 // @grant        none
+// @icon         https://cdn.prod.website-files.com/65b6a1a4a0e2af577bccce96/65ba99c1616e21b24009b86c_blum-256.png
 // @downloadURL  https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/test.js
 // @updateURL    https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/test.js
 // @homepage     https://github.com/IvanAgafonov
