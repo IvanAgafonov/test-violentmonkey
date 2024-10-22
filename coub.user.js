812f1b04c51bf1f67f969cab194f17bb75db88d1
Ivan <sudden1306@gmail.com>
2024-10-22 21:20:57 +0300

    Update and rename coub.js to coub.user.js

--- coub.user.js
+++ coub.user.js
@@ -4,8 +4,8 @@
 // @author       IvanAgafonov
 // @match        https://coub.com/*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/coub.js
-// @updateURL    https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/coub.js
+// @downloadURL  https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/coub.user.js
+// @updateURL    https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/coub.user.js
 // @homepage     https://github.com/IvanAgafonov/test-violentmonkey
 // ==/UserScript==
 
