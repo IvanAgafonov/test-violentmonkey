948ea62b056f8697fde45ade7daad81231ea602f
Ivan <sudden1306@gmail.com>
2024-10-25 17:49:04 +0300

    Update and rename tapswap_clicker.js to tapswap_clicker.user.js

--- tapswap_clicker.user.js
+++ tapswap_clicker.user.js
@@ -4,8 +4,8 @@
 // @author       IvanAgafonov
 // @match        *://*.tapswap.club/*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/tapswap_clicker.js
-// @updateURL    https://raw.githubusercontent.com/IvanAgafonov/test-violentmonkey/refs/heads/main/tapswap_clicker.js
+// @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/tapswap_clicker.user.js
+// @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/tapswap_clicker.user.js
 // @homepage     https://github.com/IvanAgafonov/test-violentmonkey
 // ==/UserScript==
 
