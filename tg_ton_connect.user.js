190655e470fec9193441747316af7662ac92a5af
Ivan <sudden1306@gmail.com>
2024-10-31 07:33:49 +0300

    Create tg_ton_connect.user.js

---
+++ tg_ton_connect.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         tg ton connect
+// @version      0.1
+// @author       IvanAgafonov
+// @match        https://web.telegram.org/k*
+// @grant        none
+// @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/tg_ton_connect.user.js
+// @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/tg_ton_connect.user.js
+// @homepage     https://github.com/IvanAgafonov/test-violentmonkey
+// ==/UserScript==
+
+
+async function connect() {
+
+  var up = Array.from(document.querySelectorAll("div div div button.popup-button.btn.primary.rp span")).filter(el => el.textContent.includes("Launch"));
+  if (up.length != 0){
+    up[0].click();
+  }
+
+  up = Array.from(document.querySelectorAll("div button.btn-primary.btn-color-primary.web-app-button")).filter(el => el.textContent.includes("Connect") ||
+                                                                                                               el.textContent.includes("Привязать кошелёк"));
+  if (up.length != 0){
+    up[0].click();
+  }
+
+}
+
+
+function initializeScript() {
+
+    connect()
+    setTimeout(initializeScript, 7000);
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeScript);
+} else {
+    setTimeout(initializeScript, 5000);
+}
