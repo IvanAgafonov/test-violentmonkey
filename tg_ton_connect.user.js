2e0a502f988b96fc5ca7f9d63d0df5ea5306f7da
Ivan <sudden1306@gmail.com>
2024-11-01 19:43:23 +0300

    Update tg_ton_connect.user.js

--- tg_ton_connect.user.js
+++ tg_ton_connect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         tg ton connect
-// @version      0.2
+// @version      0.3
 // @author       IvanAgafonov
 // @match        https://web.telegram.org/k*
 // @grant        none
@@ -12,16 +12,22 @@
 
 async function connect() {
 
-  var up = Array.from(document.querySelectorAll("div div div button.popup-button.btn.primary.rp span")).filter(el => el.textContent.includes("Launch"));
-  if (up.length != 0){
-    up[0].click();
-  }
+  var up = Array.from(document.querySelectorAll("spa.peer-title")).filter(el => el.textContent == 'Wallet');
+  var up2 = document.evaluate("(//div[contains(@class,'BrowserHeaderTabTitle') and text()='Wallet'])[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  if (up.length != 0 || up2){
 
-  up = Array.from(document.querySelectorAll("div button.btn-primary.btn-color-primary.web-app-button")).filter(el => el.textContent.includes("Connect") ||
-                                                                                                               el.textContent.includes("Confirm") ||
-                                                                                                               el.textContent.includes("Привязать кошелёк"));
-  if (up.length != 0){
-    up[0].click();
+    up = Array.from(document.querySelectorAll("div div div button.popup-button.btn.primary.rp span")).filter(el => el.textContent.includes("Launch"));
+    if (up.length != 0){
+      up[0].click();
+    }
+
+    up = Array.from(document.querySelectorAll("div button.btn-primary.btn-color-primary.web-app-button")).filter(el => el.textContent.includes("Connect") ||
+                                                                                                                 el.textContent.includes("Подключить") ||
+                                                                                                                 el.textContent.includes("Confirm") ||
+                                                                                                                 el.textContent.includes("Привязать кошелёк"));
+    if (up.length != 0){
+      up[0].click();
+    }
   }
 
 }
