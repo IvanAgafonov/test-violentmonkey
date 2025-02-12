abf5a20eade54adc5b1a49b0dd565a3dbfbd59cb
Ivan <sudden1306@gmail.com>
2025-02-12 23:17:17 +0300

    Update onton_sbt.user.js

--- onton_sbt.user.js
+++ onton_sbt.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Onton sbt
-// @version      0.14
+// @version      0.15
 // @author       IvanAgafonov
 // @match        https://app.onton.live/*
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/onton_sbt.user.js
@@ -122,6 +122,18 @@ async function autoBuy4() {
     inputElement.dispatchEvent(new Event('change'));
 }
 
+async function autoBuy5() {
+    await connectWallet();
+    const inputElement = document.querySelector("input[placeholder='Event password']");
+    inputElement.click();
+    await sleep(getRandomDelay(100, 210));
+    inputElement.value = "layerzero";
+    inputElement.setAttribute('value', "layerzero");
+
+    inputElement.dispatchEvent(new Event('input', { bubbles: true }));
+    inputElement.dispatchEvent(new Event('change'));
+}
+
 
 function initializeScript() {
     if (document.URL.includes("66d463c1-2e53-43b0-901b-7a34fd07e9f7")) {
@@ -138,6 +150,11 @@ function initializeScript() {
       console.log('START ton society claim 3')
 
       setTimeout(autoBuy3, getRandomDelay(10000, 10050));
+    }
+    if (document.URL.includes("17d0fffc-caae-4d45-9683-037964ffeeca")) {
+      console.log('START ton society claim 2')
+
+      setTimeout(autoBuy5, getRandomDelay(10000, 10050));
     }
 }
 
