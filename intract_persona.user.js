61b06495b818bbeefc94ef724b53932bfea2ad0c
Ivan <sudden1306@gmail.com>
2025-03-06 03:11:00 +0300

    Create intract_persona.user.js

---
+++ intract_persona.user.js
@@ -0,0 +1,136 @@
+// ==UserScript==
+// @name         intract persona
+// @version      0.1
+// @author       IvanAgafonov
+// @match        https://persona.intract.io/*
+// @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/intract_persona.user.js
+// @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/intract_persona.user.js
+// @homepage     https://github.com/IvanAgafonov/test-violentmonkey
+// @grant        none
+
+// ==/UserScript==
+
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function getRandomDelay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Триггеры событий
+function triggerEvents(element) {
+  const events = [
+      new MouseEvent('mouseover', {
+  'view': window,
+  'bubbles': true,
+  'cancelable': true
+}),
+      new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
+      new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseup', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('click', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerout', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerleave', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseout', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new MouseEvent('mouseleave', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 })
+  ];
+
+  events.forEach((event, index) => {
+      setTimeout(() => element.dispatchEvent(event), index * 100);
+  });
+}
+
+function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function connectWallet(){
+  var up2 = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Wallet On")
+  if (up2.length != 0){
+    triggerEvents(up2[0]);
+    await sleep(getRandomDelay(3000, 4100));
+    up2 = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("Open Wallet in Telegram"))
+    if (up2.length != 0){
+      triggerEvents(up2[0]);
+      await sleep(getRandomDelay(10000, 21000));
+    }
+  }
+}
+
+async function autoBuy() {
+
+
+  var up = Array.from(document.querySelectorAll("button span")).filter(el => el.textContent.includes("Get Started"));
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(7000, 9000));
+  }
+
+  up = Array.from(document.querySelectorAll("button span")).filter(el => el.textContent.includes("Next"));
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(9000, 14000));
+  }
+
+  up = Array.from(document.querySelectorAll("button span")).filter(el => el.textContent.includes("Proceed"));
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(11000, 14000));
+  }
+
+  const paintButton = document.evaluate("(//span[text()='Verify'])[last()]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  if (paintButton) {
+    triggerEvents(paintButton);
+  }
+
+  up = Array.from(document.querySelectorAll("button span")).filter(el => el.textContent.includes("Mint"));
+  if (up.length > 1){
+    triggerEvents(up[1]);
+    await sleep(getRandomDelay(5000, 6000));
+  }
+
+  up = Array.from(document.querySelectorAll("button span")).filter(el => el.textContent.includes("Upgrade"));
+  if (up.length > 1){
+    triggerEvents(up[1]);
+    await sleep(getRandomDelay(5000, 6000));
+  }
+
+  // var up = Array.from(document.querySelectorAll("button")).filter(el => el.className.includes("_achievement_claim_button_container_"));
+  // shuffle(up);
+  // for (const item of up) {
+  //   console.log(item);
+  //   triggerEvents(item);
+  //   await sleep(getRandomDelay(2000, 3000));
+  // }
+
+}
+
+
+function initializeScript() {
+
+    console.log('START claim ')
+
+    setTimeout(autoBuy, getRandomDelay(10000, 15050));
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeScript);
+} else {
+    initializeScript();
+}
