8ee33862a66d92a8f7f591c55ea8a4bf80cf84f9
Ivan <sudden1306@gmail.com>
2025-03-23 14:56:53 +0300

    Update treasury.user.js

--- treasury.user.js
+++ treasury.user.js
@@ -202,7 +202,7 @@ async function autoBuy() {
 
 function initializeScript() {
 
-    console.log('START claim   ')
+    console.log('START claim    ')
 
     setTimeout(autoBuy, getRandomDelay(10000, 13050));
 }
