5e1da4e0776476fa682a08bc2b4cbef4dc6e224f
Ivan <sudden1306@gmail.com>
2025-03-23 18:23:23 +0300

    Update cloudflare.user.js

--- cloudflare.user.js
+++ cloudflare.user.js
@@ -1,17 +1,107 @@
 // ==UserScript==
-// @name         cloudflare claim
-// @version      0.15
+// @name         cloudflare
+// @version      0.1
 // @author       IvanAgafonov
-// @match        https://challenges.cloudflare.com/*
-// @grant        none
+// @match        https://testnet.monad.xyz/
+// @match        https://app.layer3.xyz/quests
+// @match        https://app.nodepay.ai/login
+// @match        https://app.nodepay.ai/dashboard
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/cloudflare.user.js
 // @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/cloudflare.user.js
 // @homepage     https://github.com/IvanAgafonov/test-violentmonkey
+// @grant        none
+// @run-at document-start
 // ==/UserScript==
 
 
+
+async function autoBuy() {
+
+  // var up = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("Connect Wallet"));
+  // if (up.length != 0){
+  //   triggerEvents(up[0]);
+  //   await sleep(getRandomDelay(2000, 3100));
+
+
+
+const i = setInterval(()=>{
+  if (window.turnstile)
+   {
+  console.log('success!!')
+  clearInterval(i)
+       window.turnstile.render = (a,b) => {
+        let params = {
+              sitekey: b.sitekey,
+              pageurl: window.location.href,
+              data: b.cData,
+              pagedata: b.chlPageData,
+              action: b.action,
+              userAgent: navigator.userAgent,
+              json: 1
+          };
+          console.log('intercepted-params:' + JSON.stringify(params));
+          window.cfCallback = b.callback;}
+  }
+  },10)
+
+  // var up = querySelectorAllShadows('input'); // all `td`s in body
+  // console.log(up);
+  // if (up.length != 0) {
+  //   triggerEvents(up[1]);
+  //   up[1].click();
+  //   await sleep(getRandomDelay(4000, 4100));
+  // }
+  // }
+
+  // var up = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("CLAIM"));
+  // if (up.length != 0){
+  //   triggerEvents(up[0]);
+  //   await sleep(getRandomDelay(6000, 7100));
+  // }
+
+}
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+autoBuy();
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function getRandomDelay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Триггеры событий
 function triggerEvents(element) {
   const events = [
+      new MouseEvent('mouseover', {
+  'view': window,
+  'bubbles': true,
+  'cancelable': true
+}),
       new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
       new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
       new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
@@ -26,6 +116,23 @@ function triggerEvents(element) {
   events.forEach((event, index) => {
       setTimeout(() => element.dispatchEvent(event), index * 100);
   });
+}
+
+function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function connectWallet(){
+  var up2 = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Wallet On")
+  if (up2.length != 0){
+    triggerEvents(up2[0]);
+    await sleep(getRandomDelay(3000, 4100));
+    up2 = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("Open Wallet in Telegram"))
+    if (up2.length != 0){
+      triggerEvents(up2[0]);
+      await sleep(getRandomDelay(10000, 21000));
+    }
+  }
 }
 
 
@@ -43,58 +150,18 @@ function querySelectorAllShadows(selector, el = document.body) {
   return result.concat(childResults).flat();
 }
 
-function getRandomDelay(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-async function autoBuy() {
-
-  // var up = Array.from(document.querySelectorAll("input[type='checkbox']"));
-  var up = Array.from(querySelectorAllShadows("input[type='checkbox']"));
-    if (up.length != 0){
-    triggerEvents(up[0]);
-    // await sleep(getRandomDelay(10500, 14000));
-  }
-  var up2 = Array.from(document.querySelectorAll("input[type='checkbox']"));
-    if (up2.length != 0){
-    triggerEvents(up2[0]);
-    // await sleep(getRandomDelay(10500, 14000));
-  }
-  const paintButton = document.evaluate("//*input[type='checkbox']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    if (paintButton){
-    triggerEvents(paintButton);
-    // await sleep(getRandomDelay(10500, 14000));
-  }
-  const paintButton2 = document.evaluate("//*[local-name() = 'input']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    if (paintButton2){
-    triggerEvents(paintButton2);
-    // await sleep(getRandomDelay(10500, 14000));
-  }
-
-  const shadowHost = document.querySelector('body');
-  const shadowRoot = shadowHost.shadowRoot;
-  const elementInShadowRoot = shadowRoot.querySelector("input[type='checkbox']");
-
-
-  if (elementInShadowRoot){
-    triggerEvents(elementInShadowRoot);
-    // await sleep(getRandomDelay(10500, 14000));
-  }
-}
-
-
-
 
 
 
 function initializeScript() {
 
-    console.log('START cloudflare claim')
-
-    setTimeout(autoBuy, getRandomDelay(5300, 8500));
+    console.log('START claim ')
+    autoBuy();
+    for(var j=0; j < 1300;j++) {
+      setTimeout(autoBuy, getRandomDelay(5*j, 6*j));
+    }
 }
-
+autoBuy();
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', initializeScript);
 } else {
