27acebf8da59add10ad76c92982d45af6fe82537
Ivan <sudden1306@gmail.com>
2025-03-25 14:41:17 +0300

    Update dinero.user.js

--- dinero.user.js
+++ dinero.user.js
@@ -1,8 +1,11 @@
 // ==UserScript==
 // @name         dinero
-// @version      0.11
+// @version      0.12
 // @author       IvanAgafonov
 // @match        https://abstract.dinero.xyz/*
+// @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/dinero.user.js
+// @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/dinero.user.js
+// @homepage     https://github.com/IvanAgafonov/test-violentmonkey
 // @grant        none
 
 // ==/UserScript==
