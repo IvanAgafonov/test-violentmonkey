5acdcf21b799929c8f52edb48308b9206e27edd7
Ivan <sudden1306@gmail.com>
2025-04-14 15:49:12 +0200

    Update treasury.user.js

--- treasury.user.js
+++ treasury.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         treasury
-// @version      0.36
+// @version      0.37
 // @author       IvanAgafonov
 // @match        https://cdn.thetreasury.io/*
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/treasury.user.js
@@ -181,6 +181,11 @@ async function autoBuy() {
     up = Array.from(document.querySelectorAll("img[src='/images/icons/social/basement_icon_day_3.png']"));
     if (up.length == 2){
       mytext = "Man in black";
+    }
+
+    up = Array.from(document.querySelectorAll("img[src='/images/icons/social/icon_day_5.png']"));
+    if (up.length == 2){
+      mytext = "Balloon";
     }
 
     up = Array.from(document.querySelectorAll("img[src='/images/icons/social/basement_icon_day2.png']"));
