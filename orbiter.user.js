2cc4c36e0da7a89ca16ac7311c6e527bb9779bc5
Ivan <sudden1306@gmail.com>
2025-05-16 01:39:41 +0300

    Update orbiter.user.js

--- orbiter.user.js
+++ orbiter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         orbiter
-// @version      0.1
+// @version      0.11
 // @author       IvanAgafonov
 // @match        https://www.orbiter.finance/?channel=0x3c0ed9ab1a12ba804900da2807c09b8afec10f67
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/orbiter.user.js
@@ -168,7 +168,7 @@ function initializeScript() {
 
     console.log('START claim  ')
 
-    setTimeout(autoBuy, getRandomDelay(13000, 13050));
+    setTimeout(autoBuy, getRandomDelay(16500, 17050));
 }
 
 if (document.readyState === 'loading') {
