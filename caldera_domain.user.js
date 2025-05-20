a1089ebb0411ae3f371fced6936c27b0ed1aa39d
Ivan <sudden1306@gmail.com>
2025-05-20 05:09:34 +0300

    Create caldera_domain.user.js

---
+++ caldera_domain.user.js
@@ -0,0 +1,174 @@
+// ==UserScript==
+// @name         caldera domain
+// @version      0.1
+// @author       IvanAgafonov
+// @match        https://catalyst.caldera.xyz/domain
+// @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/caldera_domain.user.js
+// @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/caldera_domain.user.js
+// @homepage     https://github.com/IvanAgafonov/test-violentmonkey
+// @grant        none
+
+// ==/UserScript==
+
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function getRandomDelay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function querySelectorAllShadows(selector, el = document.body) {
+  // recurse on childShadows
+  const childShadows = Array.from(el.querySelectorAll('*')).
+    map(el => el.shadowRoot).filter(Boolean);
+
+  // console.log('[querySelectorAllShadows]', selector, el, `(${childShadows.length} shadowRoots)`);
+
+  const childResults = childShadows.map(child => querySelectorAllShadows(selector, child));
+
+  // fuse all results into singular, flat array
+  const result = Array.from(el.querySelectorAll(selector));
+  return result.concat(childResults).flat();
+}
+
+
+// Триггеры событий
+function triggerEvents(element) {
+  const events = [
+      new MouseEvent('mouseover', {
+  'view': window,
+  'bubbles': true,
+  'cancelable': true
+}),
+      new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
+      new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseup', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('click', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerout', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerleave', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseout', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new MouseEvent('mouseleave', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 })
+  ];
+
+  events.forEach((event, index) => {
+      setTimeout(() => element.dispatchEvent(event), index * 100);
+  });
+}
+
+function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function connectWallet(){
+  var up2 = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Wallet On" || el.textContent == "Wallet in")
+  if (up2.length != 0){
+    triggerEvents(up2[0]);
+    await sleep(getRandomDelay(3000, 4100));
+    up2 = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("Open Wallet in Telegram") || el.textContent.includes("Connect Wallet in Telegram on desktop"))
+    if (up2.length != 0){
+      triggerEvents(up2[0]);
+      await sleep(getRandomDelay(10000, 21000));
+    }
+  }
+  await sleep(getRandomDelay(2000, 3100));
+}
+
+async function autoBuy() {
+
+  var up = Array.from(document.querySelectorAll("div button")).filter(el => el.textContent == "Sign in");
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(3000, 4000));
+  }
+
+
+  up = querySelectorAllShadows('span').filter(el => el.textContent.includes("Rabby"));
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(26000, 26010));
+  }
+
+  up = Array.from(document.querySelectorAll("div input"));
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(1000, 2000));
+  }
+
+
+    var nameList = [
+                'Time','Past','Future','Dev',
+                'Fly','Flying','Soar','Soaring','Power','Falling',
+                'Fall','Jump','Cliff','Mountain','Rend','Red','Blue',
+                'Green','Yellow','Gold','Demon','Demonic','Panda','Cat',
+                'Kitty','Kitten','Zero','Memory','Trooper','XX','Bandit',
+                'Fear','Light','Glow','Tread','Deep','Deeper','Deepest',
+                'Mine','Your','Worst','Enemy','Hostile','Force','Video',
+                'Game','Donkey','Mule','Colt','Cult','Cultist','Magnum',
+                'Gun','Assault','Recon','Trap','Trapper','Redeem','Code',
+                'Script','Writer','Near','Close','Open','Cube','Circle',
+                'Geo','Genome','Germ','Spaz','Shot','Echo','Beta','Alpha',
+                'Gamma','Omega','Seal','Squid','Money','Cash','Lord','King',
+                'Duke','Rest','Fire','Flame','Morrow','Break','Breaker','Numb',
+                'Ice','Cold','Rotten','Sick','Sickly','Janitor','Camel','Rooster',
+                'Sand','Desert','Dessert','Hurdle','Racer','Eraser','Erase','Big',
+                'Small','Short','Tall','Sith','Bounty','Hunter','Cracked','Broken',
+                'Sad','Happy','Joy','Joyful','Crimson','Destiny','Deceit','Lies',
+                'Lie','Honest','Destined','Bloxxer','Hawk','Eagle','Hawker','Walker',
+                'Zombie','Sarge','Capt','Captain','Punch','One','Two','Uno','Slice',
+                'Slash','Melt','Melted','Melting','Fell','Wolf','Hound',
+                'Legacy','Sharp','Dead','Mew','Chuckle','Bubba','Bubble','Sandwich','Smasher','Extreme','Multi','Universe','Ultimate','Death','Ready','Monkey','Elevator','Wrench','Grease','Head','Theme','Grand','Cool','Kid','Boy','Girl','Vortex','Paradox'
+            ];
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var finalName = nameList[Math.floor( Math.random() * nameList.length )] + nameList[Math.floor( Math.random() * nameList.length )] + alphabet[Math.floor( Math.random() * alphabet.length )];
+
+  up = document.querySelector("div input");
+  if (up) {
+    up.setAttribute('value', finalName);
+    up.dispatchEvent(new Event('input', { bubbles: true }));
+    up.dispatchEvent(new Event('change'));
+    await sleep(getRandomDelay(2000, 2100));
+  }
+
+  up = Array.from(document.querySelectorAll("path[d='m12 5 7 7-7 7']"));
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(5000, 6000));
+  }
+
+  up = Array.from(document.querySelectorAll("div button")).filter(el => el.textContent == "Register Domain");
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(3000, 4000));
+  }
+
+
+}
+
+
+function initializeScript() {
+
+    console.log('START claim  ')
+
+    setTimeout(autoBuy, getRandomDelay(13000, 13050));
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeScript);
+} else {
+    initializeScript();
+}
