da5f5ef9b25657e2c38b8fef5604b0dd594e3740
Ivan <sudden1306@gmail.com>
2025-05-23 01:13:31 +0300

    Create primegiftsbot.user.js

---
+++ primegiftsbot.user.js
@@ -0,0 +1,108 @@
+// ==UserScript==
+// @name         primegiftsbot
+// @version      0.1
+// @author       IvanAgafonov
+// @match        https://bot.primegifts.org/*
+// @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/primegiftsbot.user.js
+// @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/primegiftsbot.user.js
+// @homepage     https://github.com/IvanAgafonov/test-violentmonkey
+// @grant        none
+
+// ==/UserScript==
+
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function getRandomDelay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Триггеры событий
+function triggerEvents(element) {
+  const events = [
+      new MouseEvent('mouseover', {
+  'view': window,
+  'bubbles': true,
+  'cancelable': true
+}),
+      new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
+      new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseup', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('click', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerout', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerleave', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseout', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new MouseEvent('mouseleave', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 })
+  ];
+
+  events.forEach((event, index) => {
+      setTimeout(() => element.dispatchEvent(event), index * 100);
+  });
+}
+
+function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function connectWallet(){
+  var up2 = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Wallet On" || el.textContent == "Wallet in")
+  if (up2.length != 0){
+    triggerEvents(up2[0]);
+    await sleep(getRandomDelay(3000, 4100));
+    up2 = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("Open Wallet in Telegram") || el.textContent.includes("Connect Wallet in Telegram on desktop"))
+    if (up2.length != 0){
+      triggerEvents(up2[0]);
+      await sleep(getRandomDelay(10000, 21000));
+    }
+  }
+  await sleep(getRandomDelay(2000, 3100));
+}
+
+async function autoBuy() {
+
+
+  var up = Array.from(document.querySelectorAll("button div")).filter(el => el.textContent == "Check" );
+  if (up.length > 2){
+    triggerEvents(up[2]);
+    await sleep(getRandomDelay(2000, 3000));
+  }
+  if (up.length > 1){
+    triggerEvents(up[1]);
+    await sleep(getRandomDelay(2000, 3000));
+  }
+  if (up.length > 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(2000, 3000));
+  }
+
+}
+
+
+function initializeScript() {
+
+    console.log('START claim         ')
+
+    setTimeout(autoBuy, getRandomDelay(13900, 13950));
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeScript);
+} else {
+    initializeScript();
+}
