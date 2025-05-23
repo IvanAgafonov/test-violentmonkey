c0d4f820f1b5bb7485d4e794f8bd66fde93b2b88
Ivan <sudden1306@gmail.com>
2025-05-24 02:33:45 +0300

    Update gate_abstract_nft.user.js

--- gate_abstract_nft.user.js
+++ gate_abstract_nft.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         gate_abstract_nft
-// @version      0.1
+// @version      0.11
 // @author       IvanAgafonov
 // @match        https://www.gate.io/web3/activities/collect-in-the-moment-digital-collectible-with-oracle-red-bull-racing
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/gate_abstract_nft.user.js
@@ -114,7 +114,7 @@ async function autoBuy() {
   console.log(paintButton);
   if (paintButton) {
     triggerEvents(paintButton);
-    await sleep(getRandomDelay(20000, 21000));
+    await sleep(getRandomDelay(24000, 25000));
   }
 
   paintButton = document.evaluate("(//span[text()='Free']//parent::div//parent::div//parent::div//div[text()='Collect'])[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
