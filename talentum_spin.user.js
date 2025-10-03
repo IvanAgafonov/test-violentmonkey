dd1c7a53532abf66180d4551dad30a5a4b8b99a5
Ivan <sudden1306@gmail.com>
2025-10-03 18:10:48 +0300

    Create talentum_spin.user.js

---
+++ talentum_spin.user.js
@@ -0,0 +1,107 @@
+// ==UserScript==
+// @name         talentum_spin
+// @version      0.1
+// @author       IvanAgafonov
+// @match        https://monad.talentum.id/need-for-spin
+// @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/talentum_spin.user.js
+// @updateURL    https://github.com/IvanAgafonov/test-violentmonkey/raw/main/talentum_spin.user.js
+// @homepage     https://github.com/IvanAgafonov/test-violentmonkey
+// @grant        none
+
+// ==/UserScript==
+
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function getRandomDelay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Триггеры событий
+function triggerEvents(element) {
+  const events = [
+      new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
+      new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseup', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('click', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerout', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerleave', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseout', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new MouseEvent('mouseleave', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 })
+  ];
+
+  events.forEach((event, index) => {
+      setTimeout(() => element.dispatchEvent(event), index * 100);
+  });
+}
+
+function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function connectWallet(){
+  var up2 = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Wallet On")
+  if (up2.length != 0){
+    triggerEvents(up2[0]);
+    await sleep(getRandomDelay(3000, 4100));
+    up2 = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("Open Wallet in Telegram"))
+    if (up2.length != 0){
+      triggerEvents(up2[0]);
+      await sleep(getRandomDelay(10000, 21000));
+    }
+  }
+}
+
+async function autoBuy() {
+
+  window.scrollTo(0, document.body.scrollHeight);
+  var paintButton = document.evaluate("//div[text()='Spin']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  if (paintButton) {
+    triggerEvents(paintButton);
+    await sleep(getRandomDelay(6000, 8000));
+  }
+
+  var up = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Collect")
+  for (const item of up) {
+    triggerEvents(item);
+    await sleep(getRandomDelay(2000, 3000));
+  }
+
+  up = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Claim ")
+  for (const item of up) {
+    triggerEvents(item);
+    await sleep(getRandomDelay(2000, 3000));
+  }
+
+
+}
+
+
+function initializeScript() {
+
+    console.log('START claim ')
+
+    setTimeout(autoBuy, getRandomDelay(13000, 19050));
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeScript);
+} else {
+    initializeScript();
+}
