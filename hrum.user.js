a12cb8c901e4f322d8bf7c65bbe82c6eeb9fc1d5
Ivan <sudden1306@gmail.com>
2025-10-12 10:08:25 +0300

    Update hrum.user.js

--- hrum.user.js
+++ hrum.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Hrum
-// @version      0.11
+// @version      0.13
 // @author       IvanAgafonov
 // @match        https://game.hrum.me/*
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/hrum.user.js
@@ -35,11 +35,6 @@ function getRandomDelay(min, max) {
 // Триггеры событий
 function triggerEvents(element) {
   const events = [
-      new MouseEvent('mouseover', {
-  'view': window,
-  'bubbles': true,
-  'cancelable': true
-}),
       new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
       new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
       new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
