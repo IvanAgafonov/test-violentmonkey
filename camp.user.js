acf2d0177a01a662515255cdd09a4d9f8a8a22af
Ivan <sudden1306@gmail.com>
2025-10-12 09:52:38 +0300

    Update camp.user.js

--- camp.user.js
+++ camp.user.js
@@ -49,11 +49,6 @@ function querySelectorAllShadows(selector, el = document.body) {
 // Триггеры событий
 function triggerEvents(element) {
   const events = [
-      new MouseEvent('mouseover', {
-  'view': window,
-  'bubbles': true,
-  'cancelable': true
-}),
       new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
       new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
       new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
