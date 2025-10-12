8cb16122474fff9971d5e3159d07279b3ccfbac1
Ivan <sudden1306@gmail.com>
2025-10-12 10:49:24 +0300

    Update superchain.user.js

--- superchain.user.js
+++ superchain.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         superchain
-// @version      0.12
+// @version      0.13
 // @author       IvanAgafonov
 // @match        https://account.superchain.eco/*
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/superchain.user.js
@@ -49,11 +49,6 @@ function querySelectorAllShadows(selector, el = document.body) {
 // Триггеры событий
 function triggerEvents(element) {
   const events = [
-      new MouseEvent('mouseover', {
-  'view': window,
-  'bubbles': true,
-  'cancelable': true
-}),
       new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
       new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
       new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
