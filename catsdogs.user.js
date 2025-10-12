ff2025e0bb4bf1ad24803085427123e7744fe44e
Ivan <sudden1306@gmail.com>
2025-10-12 09:53:17 +0300

    Update catsdogs.user.js

--- catsdogs.user.js
+++ catsdogs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         catsdogs
-// @version      0.1
+// @version      0.11
 // @author       IvanAgafonov
 // @match        https://foodz.live/*
 // @downloadURL  https://github.com/IvanAgafonov/test-violentmonkey/raw/main/catsdogs.user.js
@@ -35,11 +35,6 @@ function getRandomDelay(min, max) {
 // Триггеры событий
 function triggerEvents(element) {
   const events = [
-      new MouseEvent('mouseover', {
-  'view': window,
-  'bubbles': true,
-  'cancelable': true
-}),
       new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
       new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
       new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
